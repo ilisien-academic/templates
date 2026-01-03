7f2d1c996f8803c72f09970c4fda41a15d55de70
ilisien-academic <irl5102@psu.edu>
2026-01-03 02:50:18 +0000

    Jan 2, 2026, 9:50 PM

--- templates.py
+++ templates.py
@@ -20,7 +20,7 @@ def get_all_gh_repos():
 def sequential_numbered_repo_number(prefix):
     repos = get_all_gh_repos()
     repos_w_prefix = [repo.split("_")[-1] for repo in repos if prefix in repo]
-    return
+    return maxrepos_w_prefix
 
 def fa25_academic_naming(course_code,assignment_type,semester=LOCAL_CONFIG['current_semester']):
     '''
