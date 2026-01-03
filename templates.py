15ae80d091cc1f71768a095e04db7020a2f20f5e
ilisien-academic <irl5102@psu.edu>
2026-01-03 04:21:22 +0000

    Jan 2, 2026, 11:21 PM

--- templates.py
+++ templates.py
@@ -70,7 +70,7 @@ def setup_template_only_gitignore(repo_path):
 
 def setup_template_technical_written_hw(repo_path):
     PYTHON_VENV_DIR = 'env'
-    
+    JULIA_PACKAGES = ['CairoMakie','DifferentialEquations','Symbolics']
 
     shutil.copytree("templates\\technical_written_hw",repo_path)
 
