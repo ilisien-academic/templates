2dba54893035984d1489c2a17569ee9213813e2e
ilisien-academic <irl5102@psu.edu>
2026-01-03 02:17:18 +0000

    Jan 2, 2026, 9:17 PM

--- templates.py
+++ templates.py
@@ -26,7 +26,7 @@ def fa25_academic_naming(course_code,assignment_type,semester=LOCAL_CONFIG['curr
 
 
     if course_code not in [item for pair in semester_config['courses'].items() for item in pair]:
-        input("Could not find chosen course code in config; are you sure this is the right course? [Y/n]")
+        ("Could not find chosen course code in config; are you sure this is the right course?")
     
 
 BASE_CONFIG = {
