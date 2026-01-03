285eff56b4f505a48abebe2f5b8638d1d1c88f42
ilisien-academic <irl5102@psu.edu>
2026-01-03 01:30:16 +0000

    Jan 2, 2026, 8:30 PM

--- templates.py
+++ templates.py
@@ -8,8 +8,10 @@ def fa25_academic_naming(course_code,assignment_type,semester='sp26'):
     
     :param course_code: course code with underscore, for example "la_197" or reference to semester config, for example "la"
     :param assignment_type: type of assignment to create; warning generated if not in config
-    
+    :param semester: semester string to draw defaults from; current default is spring 2026
     '''
+    semester_config = load_semester_config()[semester]
+    
 
 BASE_CONFIG = {
     'gh_username':'ilisien-academic',
