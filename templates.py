a497a07b2bd87e9ac5dba95f53e1d13593a13fa4
ilisien-academic <irl5102@psu.edu>
2026-01-03 00:14:03 +0000

    Jan 2, 2026, 7:14 PM

--- templates.py
+++ templates.py
@@ -2,7 +2,9 @@ import os, shutil, subprocess
 from pathlib import Path
 
 CONFIG = {
-    'gh_username':'ilisien-academic'
-    'gh_key_name':''
-    'default_gh_repo_visibility'
+    'gh_username':'ilisien-academic',
+    'gh_key_name':'github.com-academic',
+    'default_gh_repo_visibility':'private',
 }
+
+def create_gh_repo()
