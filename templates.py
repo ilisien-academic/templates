8a32dc52b56b968c89632caaf9ebb0c43e9d85c0
ilisien-academic <irl5102@psu.edu>
2026-01-03 03:21:18 +0000

    Jan 2, 2026, 10:21 PM

--- templates.py
+++ templates.py
@@ -62,4 +62,5 @@ def fa25_academic_naming(course_code,assignment_type,semester=LOCAL_CONFIG['curr
     return repo_prefix + str(sequential_numbered_repo_number(repo_prefix))
 
 def create_gh_repo(repo_name,visibility=GH_CONFIG['default_gh_repo_visibility']):
-    output = subprocess.run(['gh','repo','new',repo_name,visibility])
+    output = subprocess.run(['gh','repo','new',repo_name,visibility],capture_output=True,text=True).stdout
+    
