b39ba66c9e4930abf3eb6d39b0ddff20e08608ed
ilisien-academic <irl5102@psu.edu>
2026-01-03 05:03:08 +0000

    Jan 3, 2026, 12:03 AM

--- templates.py
+++ templates.py
@@ -75,7 +75,7 @@ def setup_template_technical_written_hw(repo_path):
     subprocess.run(['python','-m','venv','env'],cwd=repo_path / 'code', check=True)
     julia_code = f'using Pkg; Pkg.activate("."); Pkg.add({str(JULIA_PACKAGES).replace("'",'"')})' #; Pkg.precompile()
     subprocess.run(['julia','--project=.','-e',julia_code], cwd=repo_path / 'code', check=True)
-    repo_path / f'{repo_path.name}.tex'
+    new_tex_path = repo_path / f'{repo_path.name}.tex'
     (repo_path / 'REPO_NAME_HERE.tex').rename(repo_path / f'{repo_path.name}.tex')
 
     long_title = input("What should I title the .tex document? (title): ")
@@ -96,7 +96,7 @@ def setup_template_technical_written_hw(repo_path):
     for placeholder, value in replacements.items():
         tex_content = tex_content.replace(placeholder,value)
     
-    with open()
+    with open("")
 
 def setup_template_mla_essay(repo_path):
     pass
